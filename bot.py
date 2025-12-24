ef4007430ab31b3f1aa2be8982d46d90050a6faf
Kirburg <mulpepe14@gmail.com>
2025-12-24 17:11:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ from aiogram.client.bot import DefaultBotProperties
 
 # ====== ENV ======
 TOKEN = os.getenv("TOKEN")
-REPORT_CHAT_ID = int(os.getenv("REPORT_CHAT_ID"))
+REPORT_CHAT_ID = None # временно
 ADMIN_ID = int(os.getenv("ADMIN_ID"))
 
 # ====== INIT ======
